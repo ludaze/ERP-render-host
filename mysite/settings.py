1ecc45045955a56e27eb6048435c010e4c274397
ludaze <56473410+ludaze@users.noreply.github.com>
2024-09-28 13:51:23 +0300

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -132,15 +133,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
-# Directory where collectstatic will gather files
-# STATIC_ROOT = BASE_DIR / 'staticfiles'
-
-# Additional directories to look for static files
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',  # This assumes your static files are directly under the base directory
-]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
     # and renames the files with unique names for each version to support long-term caching
